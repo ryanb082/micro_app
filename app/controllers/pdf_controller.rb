3eeb4f93821cc3756e6b010042a292f7fa178266
Dave Gerlach <cdgerlach@gmail.com>
2016-07-14 08:08:14 -0500

    pdf_controller: Add proper pdf fill in code and serve to user
    Based on form data passed in post request fill out the first page of the
    pdf form based on the lookup table for form_mappings.
    
    Signed-off-by: Dave Gerlach <cdgerlach@gmail.com>

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,5 +1,37 @@
+require 'pdf_forms'
+
 class PdfController < ApplicationController
+
 	def create
-		render :text => params.inspect
+		field_mapping = {
+			"pretax_income"=>:LineA,
+			"pretax_coborrow_income"=>:LineB,
+			"pretax_income_total"=>"LineC",
+			"takehome_income"=>"LineD",
+			"takehome_coborrower_income"=>"LineE",
+			"takehome_income_total"=>"LineF",
+			"rent"=>"LineG",
+			"utilities"=>"LineH",
+			"debt_payments"=>"LineI",
+			"other_expenses"=>"LineJ",
+			"savings"=>"LineK",
+			"total_spendings"=>"LineL",
+		}
+
+		form_vals = Hash.new(0)
+
+		field_mapping.each do |key, field|
+			form_vals[field] = params[key];
+		end
+
+		source_form = "#{Rails.root}/public/assets/monthly_payment_worksheet_form.pdf"
+		tmp_form = Tempfile.new('cfpb_form')
+
+		pdftk = PdfForms.new('/usr/bin/pdftk')
+
+		pdftk.fill_form source_form, tmp_form.path, form_vals, :flatten => true
+
+		send_file(tmp_form.path, filename: "cpfb_monthly_sheet_filled.pdf", type: "application/pdf")
 	end
+
 end
